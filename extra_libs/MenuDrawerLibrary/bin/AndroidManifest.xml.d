83665e8d1f8670a40481d5f81b78bd5c33aa5c26
yxf <yuxiaofei_work@126.com>
2015-01-11 20:25:41 +0800

    搭建好环境

---
+++ extra_libs/MenuDrawerLibrary/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/bin/AndroidManifest.xml : \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/AndroidManifest.xml \
---
+++ extra_libs/MenuDrawerLibrary/gen/R.java.d
@@ -0,0 +1,7 @@
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/gen/net/simonvt/menudrawer/R.java \
+ : /home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/res/values/ids.xml \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/res/values/colors.xml \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/res/values/strings.xml \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/res/values/attrs.xml \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/res/values/styles.xml \
+/home/yxf/workspace/ouwan/extra_libs/MenuDrawerLibrary/bin/AndroidManifest.xml \
