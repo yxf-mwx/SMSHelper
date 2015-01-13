6fcba1c39724cf11c1ec8b4bbad48882e6acdc0b
yxf <yuxiaofei_work@126.com>
2015-01-13 15:44:36 +0800

    tmp

--- app/src/main/java/android/com/smshelper/activity/MainActivity.java
+++ app/src/main/java/android/com/smshelper/activity/MainActivity.java
@@ -2,17 +2,24 @@ package android.com.smshelper.activity;
 
 import android.com.smshelper.R;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import net.simonvt.menudrawer.MenuDrawer;
+import net.simonvt.menudrawer.Position;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+import static android.view.View.OnClickListener;
 
+
+public class MainActivity extends ActionBarActivity implements OnClickListener, MenuDrawer.OnDrawerStateChangeListener {
 	TextView mTextView = null;
+	MenuDrawer mMenuDrawer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 		setContentView(R.layout.activity_main);
 		mTextView = (TextView) findViewById(R.id.tv1_main_activity);
 		mTextView.setOnClickListener(this);
+		initMenuDrawer();
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,5 +56,47 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 	public void onClick(View v) {
 		Intent intent = new Intent(MainActivity.this, BlackListActivity.class);
 		startActivity(intent);
+	}
+
+	private void initMenuDrawer() {
+		final int menuSize = getResources().getDisplayMetrics().widthPixels * 2 / 3;
+		mMenuDrawer = MenuDrawer.attach(
+				MainActivity.this,
+				MenuDrawer.Type.BEHIND,
+				Position.LEFT,
+				MenuDrawer.MENU_DRAG_WINDOW
+		);
+		mMenuDrawer.setTouchMode(MenuDrawer.TOUCH_MODE_FULLSCREEN);
+		mMenuDrawer.setMenuView(R.layout.fragment_leftmenu_frame);
+		mMenuDrawer.setMenuSize(menuSize);
+		mMenuDrawer.setOnDrawerStateChangeListener(this);
+	}
+
+	@Override
+	public void onDrawerStateChange(int oldState, int newState) {
+		int logoRes = 0;
+
+		if (newState == MenuDrawer.STATE_CLOSED) {
+			logoRes = R.drawable.ic_actionbar_menu;
+		} else {
+			logoRes = R.drawable.ic_actionbar_back;
+		}
+		Log.d(getClass().getName(), String.valueOf(logoRes));
+		Drawable drawable = getResources().getDrawable(logoRes);
+		if (logoRes != 0) {
+			getSupportActionBar().setLogo(drawable);
+		}
+	}
+
+	@Override
+	public void onDrawerSlide(float openRatio, int offsetPixels) {
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mMenuDrawer.setOnDrawerStateChangeListener(null);
+		mMenuDrawer = null;
 	}
 }
---
+++ app/src/main/java/android/com/smshelper/fragment/Fragment_LeftMenu.java
@@ -0,0 +1,23 @@
+package android.com.smshelper.fragment;
+
+import android.com.smshelper.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 15-1-11.
+ */
+public class Fragment_LeftMenu extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return View.inflate(getActivity(), R.layout.activity_balcklist, null);
+	}
+}
