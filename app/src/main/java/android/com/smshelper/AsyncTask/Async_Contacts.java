d63e62288c21c42550aca1dd29f350eaec37d4c1
yuxiaofei <yuxiaofei@youmi.net>
2015-03-11 22:03:02 +0800

    增加了联系人按姓名排序

--- app/src/main/java/android/com/smshelper/AsyncTask/Async_Contacts.java
+++ app/src/main/java/android/com/smshelper/AsyncTask/Async_Contacts.java
@@ -7,9 +7,18 @@ import android.content.Context;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.provider.ContactsContract;
+import android.text.TextUtils;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by yxf on 15-3-8.
@@ -25,22 +34,33 @@ public class Async_Contacts extends AsyncTask<Void, Void, List<Contact>> {
 
 	@Override
 	protected List<Contact> doInBackground(Void... params) {
-		List<Contact> result = new ArrayList<>();
+		Map<String, String> map = new HashMap();
 		ContentResolver cr = mContext.getContentResolver();
 		final String[] mContactsProjection = new String[]{
 				ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
 				ContactsContract.CommonDataKinds.Phone.NUMBER,
-				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+
 		};
 		Cursor cursor = cr.query(
 				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, mContactsProjection, null, null, null);
 		while (cursor.moveToNext()) {
 			final String phone = cursor.getString(1);
 			final String nick = cursor.getString(2);
+			map.put(phone, nick);
+		}
+		List<Contact> result = new ArrayList<>();
+		Set<Map.Entry<String, String>> entitySet = map.entrySet();
+		Iterator<Map.Entry<String, String>> it = entitySet.iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, String> mapEntry = it.next();
+			String nick = mapEntry.getValue();
+			String phone = mapEntry.getKey();
 			Contact contact = new Contact(nick, phone);
 			result.add(contact);
 		}
 		cursor.close();
+		Collections.sort(result, new PinYinComparator());
 		return result;
 	}
 
@@ -49,6 +69,37 @@ public class Async_Contacts extends AsyncTask<Void, Void, List<Contact>> {
 		super.onPostExecute(list);
 		if (mCallBack != null) {
 			mCallBack.onFinish(list);
+		}
+	}
+
+	private class PinYinComparator implements Comparator<Contact> {
+		@Override
+		public int compare(Contact lhs, Contact rhs) {
+			try {
+				return generatePinYinString(lhs.getNick()).compareTo(generatePinYinString(rhs.getNick()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		private String generatePinYinString(String s) {
+			StringBuffer sb = new StringBuffer();
+			if (!TextUtils.isEmpty(s)) {
+				for (int i = 0; i < s.length(); i++) {
+					char c = s.charAt(i);
+					String[] array = PinyinHelper.toHanyuPinyinStringArray(c);
+					if (null != array && array.length > 0) {
+						sb.append(array[0]);
+					}
+				}
+			}
+			return sb.toString();
+		}
+
+		@Override
+		public boolean equals(Object object) {
+			return false;
 		}
 	}
 }
