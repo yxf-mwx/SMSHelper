dc604c0dd9b9bcfe00650e069a99172714913139
yuxiaofei <yuxiaofei@youmi.net>
2015-03-12 10:14:53 +0800

    从联系人可以直接添加到黑名单

--- app/src/main/java/android/com/smshelper/activity/ActivityContacts.java
+++ app/src/main/java/android/com/smshelper/activity/ActivityContacts.java
@@ -1,16 +1,20 @@
 package android.com.smshelper.activity;
 
+import android.com.smshelper.AppConstant;
 import android.com.smshelper.AsyncTask.Async_Contacts;
 import android.com.smshelper.R;
 import android.com.smshelper.adapter.AdapterContacts;
 import android.com.smshelper.entity.Contact;
+import android.com.smshelper.entity.PeopleInfo;
 import android.com.smshelper.interfac.OnReadContactFinished;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +22,12 @@ import java.util.List;
 /**
  * Created by admin on 15-1-20.
  */
-public class ActivityContacts extends ActionBarActivity implements OnReadContactFinished {
+public class ActivityContacts extends ActionBarActivity implements OnReadContactFinished, View.OnClickListener {
 	private List<Contact> mList;
 
 	private ListView mLvMain;
 	private View mLayoutLoading;
+	private TextView mBtnAdd;
 	private AdapterContacts mAdapter;
 
 	@Override
@@ -32,6 +37,8 @@ public class ActivityContacts extends ActionBarActivity implements OnReadContact
 		initActionBar();
 		mLvMain = (ListView) findViewById(R.id.lv_main_common);
 		mLayoutLoading = findViewById(R.id.layout_loading_common);
+		mBtnAdd = (TextView) findViewById(R.id.tv_add_common);
+		mBtnAdd.setOnClickListener(this);
 		mList = new ArrayList<>();
 		mAdapter = new AdapterContacts(this, mList);
 		mLvMain.setAdapter(mAdapter);
@@ -70,5 +77,21 @@ public class ActivityContacts extends ActionBarActivity implements OnReadContact
 				mAdapter.notifyDataSetChanged();
 			}
 		});
+	}
+
+	@Override
+	public void onClick(View v) {
+		ArrayList<PeopleInfo> result = new ArrayList<>();
+		for (Contact contact : mList) {
+			if (contact.isCheck()) {
+				final String nick = contact.getNick();
+				final String phone = contact.getPhone();
+				result.add(new PeopleInfo(nick, phone));
+			}
+		}
+		Intent intent = new Intent();
+		intent.putParcelableArrayListExtra(AppConstant.ARGS_SELECTLIST, result);
+		setResult(AppConstant.RESULTCODE_CONTACTS, intent);
+		this.finish();
 	}
 }
