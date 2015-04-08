9656244b806fa8b16daefd2b66550f2868a4553a
yuxiaofei <yuxiaofei@youmi.net>
2015-04-08 20:31:28 +0800

    add sms adapter

--- app/src/main/java/android/com/smshelper/adapter/AdapterSMS.java
+++ app/src/main/java/android/com/smshelper/adapter/AdapterSMS.java
@@ -1,13 +1,18 @@
 package android.com.smshelper.adapter;
 
 import android.com.smshelper.R;
+import android.com.smshelper.Util.DateUtils;
 import android.com.smshelper.entity.ListItemSMS;
 import android.com.smshelper.interfac.OnItemClickListener;
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -45,14 +50,34 @@ public class AdapterSMS extends BaseAdapter {
 		if (convertView == null) {
 			holder = new ViewHolder();
 			convertView = View.inflate(mContext, R.layout.listitem_sms, null);
+			holder.checkbox = (CheckBox) convertView.findViewById(R.id.cb_sms_checkbox);
+			holder.body = (TextView) convertView.findViewById(R.id.tv_sms_body);
+			holder.personOrAddress = (TextView) convertView.findViewById(R.id.tv_sms_addressorperson);
+			holder.date = (TextView) convertView.findViewById(R.id.tv_sms_date);
+			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
+		ListItemSMS item = mListData.get(position);
+		final String person = item.getPersion();
+		final String address = item.getAddress();
+		if (TextUtils.isEmpty(person)) {
+			holder.personOrAddress.setText(address);
+		} else {
+			holder.personOrAddress.setText(person);
+		}
 
+		final Date date = new Date(item.getDate());
+		String dateStr = DateUtils.getDate(date);
+		holder.date.setText(dateStr);
+		holder.body.setText(item.getBody());
 		return convertView;
 	}
 
 	private static class ViewHolder {
-
+		TextView personOrAddress;
+		TextView date;
+		TextView body;
+		CheckBox checkbox;
 	}
 }
